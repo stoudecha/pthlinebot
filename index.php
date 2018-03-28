e7a074e08bb2c24f55f50e77eff9e44202ea48ba
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-28 16:46:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ $content = file_get_contents('php://input');// Parse JSON
 $events = json_decode($content, true);// Validate parsed JSON data
   if (!is_null($events['events'])){	// Loop through each event	
   foreach ($events['events'] as $event) {		// Reply only when message sent is in 'text' format	
-    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			// Get text sent	
-    $text ="คุณได้คิวตรวจที่ http://pth.ddns.net/tcpdf/examples/que_card_n.php?ref=MjAxOC0wMy0yODE0"	;
-      //$text = $event['message']['text'];			// Get replyToken			
-      $replyToken = $event['replyToken'];			// Build message to reply back			
-      //$messages = ['type' => 'text','text' => $text			];	
+    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			// Get text sent		
+      $text = $event['message']['text'];			// Get replyToken			
+      $replyToken = $event['replyToken'];			// Build message to reply back	
+      	 $msg ="คุณได้คิวตรวจที่ http://pth.ddns.net/tcpdf/examples/que_card_n.php?ref=MjAxOC0wMy0yODE0";	
+      $messages = [				'type' => 'text',				'text' => $text.$msg			];	
       	// Make a POST Request to Messaging API to reply to sender			
-      //$messages ="message= คุณได้คิวตรวจที่ http://pth.ddns.net/tcpdf/examples/que_card_n.php?ref=MjAxOC0wMy0yODE0";
+     
       $url = 'https://api.line.me/v2/bot/message/reply';		
       $data = [				'replyToken' => $replyToken,				'messages' => [$messages],			];		
       $post = json_encode($data);			
