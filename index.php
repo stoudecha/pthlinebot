42b7ab7698ee85b9a72f7adf18ed54b1ecbc2c7c
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-28 16:58:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,63 @@
 <?php
-$access_token ='4Twn1MlK34hhjetxAspfd8QoOYQSl3yluDVmRguMTrrPlMtcSQVlAJhe+H3aE+FhlAZ5dOB8cJZr+vnszYz8xknwzX9mHBx7mzpwasExfoE+vsJfGO5doTtd50NmZuSW04CkvI43ffrGu4mJ3uLuXwdB04t89/1O/w1cDnyilFU=';// Get POST body content
-$content = file_get_contents('php://input');// Parse JSON
-$events = json_decode($content, true);// Validate parsed JSON data
-  if (!is_null($events['events'])){	// Loop through each event	
-  foreach ($events['events'] as $event) {		// Reply only when message sent is in 'text' format	
-    if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			// Get text sent		
-      $text = $event['message']['text'];			// Get replyToken			
-      $replyToken = $event['replyToken'];			// Build message to reply back	
-      	 $msg ="คุณได้คิวตรวจที่ http://pth.ddns.net/tcpdf/examples/que_card_n.php?ref=MjAxOC0wMy0yODE0";	
-      $messages = [				'type' => 'text',				'text' => $text		];	
-      	// Make a POST Request to Messaging API to reply to sender			
-     
-      $url = 'https://api.line.me/v2/bot/message/reply';		
-      $data = [				'replyToken' => $replyToken,				'messages' => [$messages],			];		
-      $post = json_encode($data);			
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);		
-      $ch = curl_init($url);			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");	
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);			curl_close($ch);			
-      echo $result . "";		}	}}
+$API_URL = 'https://api.line.me/v2/bot/message/reply';
+$ACCESS_TOKEN = ='4Twn1MlK34hhjetxAspfd8QoOYQSl3yluDVmRguMTrrPlMtcSQVlAJhe+H3aE+FhlAZ5dOB8cJZr+vnszYz8xknwzX9mHBx7mzpwasExfoE+vsJfGO5doTtd50NmZuSW04CkvI43ffrGu4mJ3uLuXwdB04t89/1O/w1cDnyilFU=';// Get POST body content
+'; // Access Token ค่าที่เราสร้างขึ้น
+$POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+if ( sizeof($request_array['events']) > 0 )
+{
+
+ foreach ($request_array['events'] as $event)
+ {
+  $reply_message = '';
+  $reply_token = $event['replyToken'];
+
+  if ( $event['type'] == 'message' ) 
+  {
+   if( $event['message']['type'] == 'text' )
+   {
+    $text = $event['message']['text'];
+    $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว';
+   }
+   else
+    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+  
+  }
+  else
+   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+ 
+  if( strlen($reply_message) > 0 )
+  {
+   //$reply_message = iconv("tis-620","utf-8",$reply_message);
+   $data = [
+    'replyToken' => $reply_token,
+    'messages' => [['type' => 'text', 'text' => $reply_message]]
+   ];
+   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+   echo "Result: ".$send_result."\r\n";
+  }
+ }
+}
+
 echo "OK";
+
+function send_reply_message($url, $post_header, $post_body)
+{
+ $ch = curl_init($url);
+ curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+ curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+ $result = curl_exec($ch);
+ curl_close($ch);
+
+ return $result;
+}
+
 ?>
