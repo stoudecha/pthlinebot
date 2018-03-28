6dc29b04a8c227ed83ea4f30755e75dd2cde07f7
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-28 17:02:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
 <?php
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN ='4Twn1MlK34hhjetxAspfd8QoOYQSl3yluDVmRguMTrrPlMtcSQVlAJhe+H3aE+FhlAZ5dOB8cJZr+vnszYz8xknwzX9mHBx7mzpwasExfoE+vsJfGO5doTtd50NmZuSW04CkvI43ffrGu4mJ3uLuXwdB04t89/1O/w1cDnyilFU=';// Get POST body content
-'; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN ='4Twn1MlK34hhjetxAspfd8QoOYQSl3yluDVmRguMTrrPlMtcSQVlAJhe+H3aE+FhlAZ5dOB8cJZr+vnszYz8xknwzX9mHBx7mzpwasExfoE+vsJfGO5doTtd50NmZuSW04CkvI43ffrGu4mJ3uLuXwdB04t89/1O/w1cDnyilFU=';// Get POST body content // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
-
 if ( sizeof($request_array['events']) > 0 )
 {
-
  foreach ($request_array['events'] as $event)
  {
   $reply_message = '';
   $reply_token = $event['replyToken'];
-
   if ( $event['type'] == 'message' ) 
   {
    if( $event['message']['type'] == 'text' )
@@ -37,15 +32,12 @@ if ( sizeof($request_array['events']) > 0 )
     'messages' => [['type' => 'text', 'text' => $reply_message]]
    ];
    $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
    $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
    echo "Result: ".$send_result."\r\n";
   }
  }
 }
-
 echo "OK";
-
 function send_reply_message($url, $post_header, $post_body)
 {
  $ch = curl_init($url);
@@ -56,8 +48,6 @@ function send_reply_message($url, $post_header, $post_body)
  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
  $result = curl_exec($ch);
  curl_close($ch);
-
  return $result;
 }
-
 ?>
