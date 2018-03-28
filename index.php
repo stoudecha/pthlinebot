5ad145620ee7e629396a7e78d0969ec34090c884
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-28 16:29:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ echo $result;
 
 ?>
 <?php
-$access_token = '$access_token';// Get POST body content
+$access_token = $access_token;// Get POST body content
 $content = file_get_contents('php://input');// Parse JSON
 $events = json_decode($content, true);// Validate parsed JSON data
   if (!is_null($events['events'])){	// Loop through each event	
