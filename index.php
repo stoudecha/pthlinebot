be247f882d4804342b0a0c23de8bcceb20617db1
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-28 16:16:35 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php echo "hello"; 
-echo "<a href='http://pth.ddns.net/tcpdf/examples/que_card_n.php?ref=MjAxOC0wMy0yODE3'>ddddf</a>";?>
+<?php$access_token = '4Twn1MlK34hhjetxAspfd8QoOYQSl3yluDVmRguMTrrPlMtcSQVlAJhe+H3aE+FhlAZ5dOB8cJZr+vnszYz8xknwzX9mHBx7mzpwasExfoE+vsJfGO5doTtd50NmZuSW04CkvI43ffrGu4mJ3uLuXwdB04t89/1O/w1cDnyilFU=';
+$url = 'https://api.line.me/v1/oauth/verify';
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);curl_close($ch);
+echo $result;
+
+
+?>
