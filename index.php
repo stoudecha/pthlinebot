cc49fcab5732633af2362146f65df9c6b54fa06e
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-29 23:59:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -26,12 +26,13 @@ $events = json_decode($content, true);// Validate parsed JSON data
       curl_close($ch);      
       echo $result . "";    } }}
 echo "OK";
-curl -v -X GET https://api.line.me/v2/bot/profile/{userId} \
--H 'Authorization: Bearer 4Twn1MlK34hhjetxAspfd8QoOYQSl3yluDVmRguMTrrPlMtcSQVlAJhe+H3aE+FhlAZ5dOB8cJZr+vnszYz8xknwzX9mHBx7mzpwasExfoE+vsJfGO5doTtd50NmZuSW04CkvI43ffrGu4mJ3uLuXwdB04t89/1O/w1cDnyilFU=';
+
+
+curl -v -X GET -H 'Authorization: Bearer 4Twn1MlK34hhjetxAspfd8QoOYQSl3yluDVmRguMTrrPlMtcSQVlAJhe+H3aE+FhlAZ5dOB8cJZr+vnszYz8xknwzX9mHBx7mzpwasExfoE+vsJfGO5doTtd50NmZuSW04CkvI43ffrGu4mJ3uLuXwdB04t89/1O/w1cDnyilFU=' https://api.line.me/v2/bot/profile/{userId};
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('4Twn1MlK34hhjetxAspfd8QoOYQSl3yluDVmRguMTrrPlMtcSQVlAJhe+H3aE+FhlAZ5dOB8cJZr+vnszYz8xknwzX9mHBx7mzpwasExfoE+vsJfGO5doTtd50NmZuSW04CkvI43ffrGu4mJ3uLuXwdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => 'cf2b32132d0788dd0f476592226c40be
 ']);
-$response = $bot->getProfile('<userId>');
+$response = $bot->getProfile('U3b5751f42f6ef750828f9d74adb4c8ce');
 if ($response->isSucceeded()) {
     $profile = $response->getJSONDecodedBody();
     echo $profile['displayName'];
