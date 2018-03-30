a110aa0cbb59f88927fad320d7e34ea74e20f610
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-30 18:10:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -23,8 +23,8 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $link=$arrJson['events'][0]['source']['userId'];
   $arrPostData['messages'][0]['type'] = "image";
-  $arrPostData['messages'][0]['originalContentUrl'] = "http://pth.ddns.net/images/test.jpg";
-  $arrPostData['messages'][0]['previewImageUrl'] = "http://pth.ddns.net/images/test.jpg";
+  $arrPostData['messages'][0]['image'] = "http://pth.ddns.net/images/test.jpg","http://pth.ddns.net/images/test.jpg";
+ 
   
 }else if($arrJson['events'][0]['message']['text'] == "จองคิว"){
   $arrPostData = array();
