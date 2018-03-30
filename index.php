214d1bd4f1cbcd469139f51e436632d42227a221
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-30 07:14:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,11 +21,6 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
 }else if($arrJson['events'][0]['message']['text'] == "จองคิว"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "กดที่นี่ http://pth.ddns.net/que_app_form.php";
-}else if($arrJson['events'][0]['message']['text'] == "จองคิว"){
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $link=$arrJson['events'][0]['source']['userId'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "จองคิวทีนี่ http://pth.ddns.net/que_register.php?regist=".$link;
