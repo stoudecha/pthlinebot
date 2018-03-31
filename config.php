5587f548582d3738b6ea86adad375c0ea8d1e7dc
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-03-31 15:27:15 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,1 +1,19 @@
-
+<?php
+class Setting {
+	public function getChannelAccessToken(){
+		$channelAccessToken = "";
+		return $channelAccessToken;
+	}
+	public function getChannelSecret(){
+		$channelSecret = "";
+		return $channelSecret;
+	}
+	public function getApiReply(){
+		$api = "https://api.line.me/v2/bot/message/reply";
+		return $api;
+	}
+	public function getApiPush(){
+		$api = "https://api.line.me/v2/bot/message/push";
+		return $api;
+	}
+}
