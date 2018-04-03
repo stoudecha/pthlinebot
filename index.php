683e0380237e19fed551c2ea6f5d9bf30fe61669
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-04-03 14:27:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,7 @@ $bot = new Linebot();
 $to="U3b5751f42f6ef750828f9d74adb4c8ce";
 $imageUrl="http://pth.ddns.net/2018-04-17231.jpg";
 $previewImageUrl=$imageUrl;
-$bot->pushImage($to, $imageUrl,  $previewImageUrl);
-
-
+$bot->pushImage($to, $imageUrl,$previewImageUrl);
 $bot->pushText($to, $imageUrl);
 
 
