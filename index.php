5ef93f5693259f80920b7238b1637d4aa90fa099
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-04-03 13:56:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 require_once __DIR__ . '/pthbot.php';
 $bot = new Linebot();
 $text = $bot->getMessageText();
-$to="U3b5751f42f6ef750828f9d74adb4c8ce";
-$imageUrl="http://pth.ddns.net/2018-04-17231.jpg";
- $previewImageUrl=$imageUrl;
-$bot->pushImage($to, $imageUrl,  $previewImageUrl = false);
+
 
 ?>
