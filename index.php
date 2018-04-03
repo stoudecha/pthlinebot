a42bb233eb9b9618064b372ce9b4d93fce64d1af
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-04-03 14:46:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ $text = $bot->getMessageText();
 $to="U3b5751f42f6ef750828f9d74adb4c8ce";
 $imageUrl="http://pth.ddns.net/2018-04-17231.jpg";
  $previewImageUrl=$imageUrl;
-$bot->pushImage($to, $imageUrl,  $previewImageUrl = false);
+$bot->pushImage($to, $imageUrl,  $previewImageUrl);
 ?>
