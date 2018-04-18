30032e70b8b1adde2da4c5dc431ae624226508ba
stoudecha <34289867+stoudecha@users.noreply.github.com>
2018-04-18 12:08:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -126,5 +126,29 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
 
+
+$strUrl = "https://api.line.me/v2/bot/message/push";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+$arrPostData = array();
+$arrPostData['to'] = $line_id;
+$arrPostData['messages'][0]['type'] = "text";
+$arrPostData['messages'][0]['text'] = "https://youtu.be/djt6wIslyEE";
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+
+
  echo"<meta http-equiv=\"refresh\" content=\"0;URL=$link\">";
 ?>
